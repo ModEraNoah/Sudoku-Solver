f2e407161d825ec80084ab25ae79d6687b4ce661
ModEraNoah <klauck02@outlook.com>
2024-08-18 21:23:18 +0200

    fix: square-check of possibleNumbers

--- main.c
+++ main.c
@@ -7,29 +7,29 @@ typedef struct{
 	int col;
 } Coordinate;
 
-char* getPossibleNumbers(char field[9][9], Coordinate cur)
+int* getPossibleNumbers(char field[9][9], Coordinate cur)
 {
-	char *pos = malloc(sizeof(char) * 9);
-	char t[] = {1,1,1,1,1,1,1,1,1};
-	pos = t;
+	int *pos = malloc(sizeof(int) * 9);
+	for (char i = 0; i<9; i++)
+		pos[i] = 1;
 	// row
 	for (char i = 0; i < 9; i++)
 	{
-		if (field[cur.row][i]) pos[field[cur.row][i]] = 0;
+		if (field[cur.row][i]) pos[(field[cur.row][i])-1] = 0;
 	}
 	// col
 	for (char i = 0; i < 9; i++)
 	{
-		if (field[i][cur.col]) pos[field[i][cur.col]] = 0;
+		if (field[i][cur.col]) pos[(field[i][cur.col])-1] = 0;
 	}
-	// square
-	const char squareCols = cur.col / 3;
-	const char squareRows = cur.row / 3;
+	// // square
+	const int squareCols = cur.col / 3;
+	const int squareRows = cur.row / 3;
 	for (char i = 0; i < 3; i++)
 	{
-		if (field[squareRows+i][squareCols + 0]) pos[field[squareRows+i][squareCols + 0]] = 0;
-		if (field[squareRows+i][squareCols + 1]) pos[field[squareRows+i][squareCols + 0]] = 0;
-		if (field[squareRows+i][squareCols + 2]) pos[field[squareRows+i][squareCols + 2]] = 0;
+		if (field[(squareRows*3)+i][(squareCols*3) + 0]) pos[field[(squareRows*3)+i][(squareCols*3) + 0] - 1] = 0;
+		if (field[(squareRows*3)+i][(squareCols*3) + 1]) pos[field[(squareRows*3)+i][(squareCols*3) + 1] - 1] = 0;
+		if (field[(squareRows*3)+i][(squareCols*3) + 2]) pos[field[(squareRows*3)+i][(squareCols*3) + 2] - 1] = 0;
 	}
 
 	return pos;
@@ -61,43 +61,40 @@ bool solve(char field[][9], Coordinate cur)
 	if (field[cur.row][cur.col]) return true;
 	
 	// ist noch nicht gesetzt -> welche zahlen sind möglich
-	char *pos = getPossibleNumbers(field, cur);
+	int *pos = getPossibleNumbers(field, cur);
 	// f+ge alle mögl. zahlen in Array und gehe sie nacheinander durch
 	for (char i = 0; i < 9; i++) 
 	{
-		printf("current possible number: %i\n", *(pos + i));
 		if (! *(pos + i)) continue;
 
 		int tmp = field[cur.row][cur.col];
-		field[cur.row][cur.col] = *(pos + i);
+		field[cur.row][cur.col] = (*(pos + i)) * (i+1);
 		
 	// wenn keine Zahl mehr "0" (sprich frei) ist, gebe `true` zurück
 		const Coordinate next = nextFreeCell(field, cur);
 		if (next.row == -1 && next.col == -1) return true;
-		solve(field, next);
-
+		if (solve(field, next)) return true;
 		field[cur.row][cur.col] = tmp;
 	}
 	// wenn keine Zahl möglich ist, gebe `false zurück`
-
 	return false;
 }
 
 int main(void)
 {
 	char field[][9] = {
-		{0,0,0,0,0,0,0,0,0},
-		{0,1,0,0,0,0,0,0,0},
-		{0,0,0,0,0,0,0,0,0},
-		{0,0,0,0,0,0,0,0,0},
-		{0,0,0,0,0,0,0,0,0},
-		{0,0,0,0,0,9,0,0,0},
-		{0,0,0,0,0,0,0,0,0},
-		{0,0,5,0,0,0,4,0,0},
-		{0,0,0,0,0,0,0,0,0},
+		{2,1,3,0,5,7,9,0,0},
+		{0,0,5,0,0,0,3,8,0},
+		{0,0,0,3,1,0,0,5,0},
+		{3,0,2,9,0,0,6,1,7},
+		{9,0,7,2,0,0,0,0,0},
+		{4,0,0,0,0,5,0,2,0},
+		{0,3,4,5,8,9,2,0,6},
+		{0,2,6,0,7,0,0,9,0},
+		{0,0,8,6,0,4,1,0,5},
 	};
 
-	solve(field, (Coordinate) {0,0});
+	printf("success? %B\n", solve(field, (Coordinate) {0,3}));
 
 	for (char i = 0; i < 9; i++)
 	{
